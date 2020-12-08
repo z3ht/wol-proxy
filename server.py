4a79734522b269e05c89d334b8cc6725d4fe93ef
Andrew Darling <andr3w.darling@gmail.com>
2020-12-08 11:39:31 -0700

    Initial commit

---
+++ server.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+import socket
+import sys
+
+mac_address = "a8:a1:59:12:f9:3b"
+password = "open sesame"
+
+if len(sys.argv) == 3:
+    # Get "IP address of Server" and also the "port number" from
+    ip = sys.argv[1]
+    port = int(sys.argv[2])
+else:
+    print("Usage : ./server.py <arg1:server ip:this system IP 192.168.1.6> <arg2:server port:4444 >")
+    exit(1)
+
+# Create a UDP socket
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# Bind the socket to the port
+server_address = (ip, port)
+s.bind(server_address)
+print("Do Ctrl+c to exit the program !!")
+
+while True:
+    print("####### Server is listening #######")
+    data, address = s.recvfrom(4096)
+    print("\n\n 2. Server received: ", data.decode('utf-8'), "\n\n")
+    if data == password:
+        os.system(f"wol {mac_address}")
+        send_data = "Wakeup initiated..."
+    else:
+        send_data = "Incorrect password"
+    s.sendto(send_data.encode('utf-8'), address)
+    print("\n\n 1. Server sent : ", send_data,"\n\n")
